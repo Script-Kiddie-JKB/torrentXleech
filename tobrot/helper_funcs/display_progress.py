f119832bff6ce304305dee2abda8dd45f3cdaed6
Jaikishan Bagul <78461084+Script-Kiddie-JKB@users.noreply.github.com>
2021-06-08 17:11:39 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | gautamajay52
+# (c) Shrimadhav U K | gautamajay52 | 
 
 import logging
 import math
@@ -23,6 +23,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
 
 
 class Progress:
@@ -50,7 +51,7 @@ class Progress:
             [
                 [
                     InlineKeyboardButton(
-                        "❌𝘾𝘼𝙉𝘾𝙀𝙇❌",
+                        "Cancel 🚫",
                         callback_data=(
                             f"gUPcancel/{chat_id}/{mes_id}/{from_user}"
                         ).encode("UTF-8"),
@@ -61,7 +62,7 @@ class Progress:
         if self.is_cancelled:
             LOGGER.info("stopping ")
             await self._mess.edit(
-                f"😔𝙇𝙚𝙚𝙘𝙝 𝘾𝙖𝙣𝙘𝙚𝙡𝙡𝙚𝙙🔴: `{ud_type}` ({humanbytes(total)})"
+                f"😔 Cancelled/ERROR: `{ud_type}` ({humanbytes(total)})"
             )
             await self._client.stop_transmission()
 
@@ -76,25 +77,18 @@ class Progress:
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = "[{0}{1}] \n⚡𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨: {2}%\n✈𝙐𝙋𝙇𝙊𝘼𝘿𝙀𝘿: ".format(
-                "".join(
-                    [FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]
-                ),
-                "".join(
-                    [
-                        UN_FINISHED_PROGRESS_STR
-                        for i in range(10 - math.floor(percentage / 10))
-                    ]
-                ),
-                round(percentage, 2),
-            )
-
-            tmp = progress + "{0} of {1}\n🔼𝙎𝙋𝙀𝙀𝘿 : {2}/s\n⏰𝙀𝙏𝘼 : {3}\n".format(
+            progress = "<b>╭────── ⌊__UploadinG: 〘{2}%〙 📤__⌉</b>\n│ \n<b>├〖{0}{1}〗</b>\n".format(
+                ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
+                ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
+                round(percentage, 2))
+            #cpu = "{psutil.cpu_percent()}%"
+            tmp = progress +"│" + "\n**├Total 📀:**   〘<code>{1}</code>〙\n**├Done ✓ :** 〘<code>{0}</code>〙\n**├Speed** 🚀 :  〘<code>{2}/s</code>〙\n**├ETA** ⏳ :  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ @PremiumCoursesDrivee ⌉**".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
                 # elapsed_time if elapsed_time != '' else "0 s",
                 estimated_total_time if estimated_total_time != "" else "0 s",
+            #tmp += "\n│"+"\n╰── ⌊ @TGFilmZone ⌉"
             )
             try:
                 if not self._mess.photo:
@@ -119,11 +113,11 @@ def humanbytes(size):
         return ""
     power = 2 ** 10
     n = 0
-    Dic_powerN = {0: " ", 1: "𝙆𝙞", 2: "𝙈𝙞", 3: "𝙂𝙞", 4: "𝙏𝙞"}
+    Dic_powerN = {0: " ", 1: "K", 2: "M", 3: "G", 4: "Ti"}
     while size > power:
         size /= power
         n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + "𝘽"
+    return str(round(size, 2)) + " " + Dic_powerN[n] + "B"
 
 
 def TimeFormatter(milliseconds: int) -> str:
@@ -132,10 +126,10 @@ def TimeFormatter(milliseconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = (
-        ((str(days) + "𝘿, ") if days else "")
-        + ((str(hours) + "𝙃, ") if hours else "")
-        + ((str(minutes) + "𝙈, ") if minutes else "")
-        + ((str(seconds) + "𝙎, ") if seconds else "")
-        + ((str(milliseconds) + "𝙈𝙎, ") if milliseconds else "")
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else "")
     )
     return tmp[:-2]
